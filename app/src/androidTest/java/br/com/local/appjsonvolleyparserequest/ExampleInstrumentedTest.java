40c2a530ca6e583d10ea0c2b928b3e3cefe6c6db
James Ng <james_ng@ig.com.br>
2021-05-03 11:58:07 -0300

    comitado em 3/5/21 11:57

--- app/src/androidTest/java/br/com/local/appjsonvolleyparserequest/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/br/com/local/appjsonvolleyparserequest/ExampleInstrumentedTest.java
@@ -19,7 +19,7 @@ import static org.junit.Assert.*;
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
-        // Context of the app under test.jjhggggj
+        // Context of the app under test. klklklklklk
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("br.com.local.appjsonvolleyparserequest", appContext.getPackageName());
     }
